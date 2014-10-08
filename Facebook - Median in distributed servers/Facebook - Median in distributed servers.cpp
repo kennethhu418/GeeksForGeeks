4757919a6bcf6bd6a966dbe313ed749f2353b959
kennethhu418 <dragon418@126.com>
2014-10-08 22:18:25 +0800

    Update

---
+++ Facebook - Median in distributed servers/Facebook - Median in distributed servers.cpp
@@ -0,0 +1,9 @@
+//Let's say you have 10,000 servers, each with a billion integers. How do you find the median?
+//http://www.careercup.com/question?id=5175812277927936
+//http://www.quora.com/What-is-the-distributed-algorithm-to-determine-the-median-of-arrays-of-integers-located-on-different-computers
+
+int main()
+{
+	return 0;
+}
+
