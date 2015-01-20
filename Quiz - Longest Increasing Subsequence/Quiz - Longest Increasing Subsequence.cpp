604fec5e60b4a0aebcf328e3a90abc41429281fc
kennethhu <dragon418@126.com>
2015-01-20 21:45:17 +0800

    Update LIS - I am winner

--- Quiz - Longest Increasing Subsequence/Quiz - Longest Increasing Subsequence.cpp
+++ Quiz - Longest Increasing Subsequence/Quiz - Longest Increasing Subsequence.cpp
@@ -8,55 +8,6 @@
 #include <ctime>
 using namespace std;
 
-//int search(const vector<int> &arr, const vector<int> &indiceArr, int target) {
-//    int mid = 0, start = 0, end = indiceArr.size() - 1;
-//    while (start < end) {
-//        mid = ((start + end) >> 1);
-//        if (target <= arr[indiceArr[mid]])
-//            end = mid - 1;
-//        else
-//            start = mid + 1;
-//    }
-//    if (start > end || target <= arr[indiceArr[start]]) return start;
-//    return start + 1;
-//}
-
-//vector<int> LIS(vector<int> &arr) {
-//    int n = arr.size();
-//    vector<int> resultArr;
-//    if (n == 0) return resultArr;
-//
-//    vector<int> tailIndiceArr(1);
-//    vector<int> prevIndiceArr(n);
-//    prevIndiceArr[0] = -1;
-//    tailIndiceArr[0] = 0;
-//    int curTailLen = 1, insertPos = 0;
-//
-//    for (int i = 1; i < n; ++i) {
-//        insertPos = search(arr, tailIndiceArr, arr[i]);
-//        if (insertPos == tailIndiceArr.size())
-//            tailIndiceArr.push_back(i);
-//        else
-//            tailIndiceArr[insertPos] = i;
-//        if (insertPos == 0)
-//            prevIndiceArr[i] = -1;
-//        else
-//            prevIndiceArr[i] = tailIndiceArr[insertPos - 1];
-//    }
-//
-//    int curPos = tailIndiceArr[tailIndiceArr.size() - 1];
-//    while (curPos >= 0) {
-//        resultArr.push_back(arr[curPos]);
-//        curPos = prevIndiceArr[curPos];
-//    }
-//
-//    int start = 0, end = resultArr.size() - 1;
-//    while (start < end)
-//        swap(resultArr[start++], resultArr[end--]);
-//
-//    return resultArr;
-//}
-
 int search(const vector<int> &arr, int target) {
     int start = 0, end = arr.size() - 1, mid = 0;
     while (start < end) {
@@ -151,14 +102,3 @@ int main() {
     }
     return 0;
 }
-
-//int main()
-//{
-//    int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60, 80 };
-//    vector<int> dataArr(arr, arr + sizeof(arr) / sizeof(int));
-//    vector<int> result = LIS(dataArr);
-//    for (int i = 0; i < result.size(); ++i)
-//        cout << result[i] << " ";
-//    cout << endl;
-//    return 0;
-//}
